11582bf5dfc1196f13c331c413830de982c6ab7e
hafsaasaleem <shaikhhafsa215@gmail.com>
2021-05-21 13:25:20 +0500

    Azure Server using Dailogflow Fulfilment

---
+++ code05/functionapp04/functionapp04/index.js
@@ -0,0 +1,27 @@
+// module.exports = async function (context, req) {
+//     context.log('JavaScript HTTP trigger function processed a request.');
+
+//     const name = (req.query.name || (req.body && req.body.name));
+//     const responseMessage = name
+//         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+//         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+//     context.res = {
+//         // status: 200, /* Defaults to 200 */
+//         body: responseMessage
+//     };
+// }
+
+const { dialogflow } = require('actions-on-google');
+const express = require('express');
+const { createHandler } = require('azure-function-express');
+const app = dialogflow();
+app.intent('Default Welcome Intent', conv => {
+    // conv.close('Hello, Azure!');
+    conv.add("Hello from Hafsa Shaikh! This is MicroSoft BackEnd")
+});
+// Put other intent handlers here.
+const expressApp = express();
+expressApp.post('/api/functionapp04', app);
+module.exports = createHandler(expressApp);
+
